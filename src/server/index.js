6707bd15d74090ad8933e81dc7248e70e59f696d
ck <keverscedric@gmail.com>
2019-09-12 10:32:31 +0200

    connect db ok, list and add ok

--- src/server/index.js
+++ src/server/index.js
@@ -6,30 +6,23 @@
  * started at 06/09/2019
  */
 
-import express from "express";
-import path from "path";
-import mongoose from "mongoose";
+const express = require("express");
+const path = require("path");
+const mongoose = require("mongoose");
 
-const {APP_PORT} = process.env;
+const APP_PORT = 7777;
 
 const app = express();
 
-// Connect Mongodb
-/*mongoose.connect("mongodb://localhost:27017/admin", { useNewUrlParser: true });*/
+const bodyParser = require("body-parser");
+app.use(bodyParser.json()); // support json encoded bodies
+app.use(bodyParser.urlencoded({extended: true})); // support enco
 
+// Connect Mongodb
 mongoose
-    .connect("mongodb://mongo:27017/admin", {
+    .connect("mongodb://dev:dev@localhost:27017/trouvkash", {
         authSource: "admin",
-        user: "dev",
-        pass: "dev",
-        dbName: "trouvkash", // 'mydb' which is the default selected DB
         useNewUrlParser: true,
-        reconnectTries: Number.MAX_VALUE,
-        reconnectInterval: 500, // Reconnect every 500ms
-        poolSize: 10, // Maintain up to 10 socket connections
-    })
-    .then(() => {
-        console.log("Successfully connected to the database");
     })
     .catch(err => {
         console.error("Could not connect to the database. Exiting now...", err);
@@ -37,21 +30,21 @@ mongoose
     });
 
 // Schema terminals
-/*
 const terminalSchema = new mongoose.Schema({
-    latitude: 'number',
-    longitude: 'number',
-    adress: "string",
-    bank: "ObjectID",
-    empty: "Boolean",
-    created_at: "string",
-    update_at: "string",
-    deleted_at: "string"
-});
-*/
-const terminalSchema = new mongoose.Schema({
-    latitude: "number",
-    longitude: "number",
+    latitude: {
+        type: Number,
+        required: true,
+    },
+    longitude: {
+        type: Number,
+        required: true,
+    },
+    address: String,
+    bank: mongoose.Schema.Types.ObjectId,
+    empty: Boolean,
+    created_at: String,
+    update_at: String,
+    deleted_at: String,
 });
 const terminals = mongoose.model("terminal", terminalSchema);
 
@@ -64,26 +57,51 @@ app.get("/hello", (req, res) => {
 // List all atm
 app.get("/atm", (req, res) => {
     console.log(`(${req.method.toUpperCase()})) ${req.url}`);
-    terminals.findOne({latitude: 51.0569}, ok => console.log(ok));
-    res.send("no");
+    terminals.find({}, (err, ok) => {
+        if (err) {
+            res.send(
+                JSON.stringify({
+                    error: err,
+                    message: "We cannot retrieve the ATM list",
+                }),
+            );
+        }
+        res.send(JSON.stringify(ok));
+    });
 });
 
 app.get("/atm/[0-9A-Za-z]+", (req, res) => {
     console.log(`(${req.method.toUpperCase()})) ${req.url}`);
-    // Grab ID
-    // req.url = /atm/id
-    // req.url.split('/') = [ '', 'atm', 'id' ]
-    // req.url.split('/').length = la grandeur du tableau
-    // Pour avoir le dernier élément il suffit de faire grandeur du tableau - 1
-    //let urlArray = req.url.split('/')
-    //let urlArraySize = req.url.split('/').length
-    //res.send("L'id est " + urlArray[urlArraySize - 1]);
-    res.send(`L'id est ${req.url.split("/")[2]}`);
-});
-app.post("/atm/[0-9]+", (req, res) => {
-    console.log(`(${req.method.toUpperCase()})) ${req.url}`);
 
-    res.send(`L'id est ${req.url.split("/")[2]}`);
+    // eslint-disable-next-line prettier/prettier
+    terminals.findOne({ _id: req.url.split("/")[2] }, (err, ok) => {
+        if (err) {
+            res.send(
+                JSON.stringify({
+                    error: err,
+                    message: `We cannot retrieve the details of ATM ${
+                        req.url.split("/")[2]
+                    }`,
+                }),
+            );
+        }
+        res.send(JSON.stringify(ok));
+    });
+});
+app.post("/atm/[0-9A-Za-z]+", (req, res) => {
+    console.log(`(${req.method.toUpperCase()}))${req.url}`); // choper es param du post
+    const latitude = req.body.latitude;
+    const longitude = req.body.longitude;
+    terminals(
+        {
+            latitude,
+            longitude,
+        },
+        err => {
+            console.log(err);
+        },
+    ).save();
+    res.send(`${latitude}+${longitude}`);
 });
 
 app.listen(APP_PORT, () =>
